9621df2bf8079b76cf099d2a2f998030a3dd3567
INS140 <IsaacNSaettel@gmail.com>
2023-03-16 03:41:26 -0700

    styles

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,27 @@ import SearchBar from './SearchBar'
 import Gallery from './Gallery'
 
 export default function App() {
-  const [search, setSearch] = useState('Slipknot')
+  const [search, setSearch] = useState('')
   const [message, setMessage] = useState('Search for Music!!!')
   const [data, setData] = useState([])
 
   useEffect(() => {
     async function fetchData() {
-      try {
-        const res = await fetch(`https://itunes.apple.com/search?term=${search}`)
-        const resData = await res.json()
-        console.log(resData.results[0])
-        if (resData.results.length > 0) {
-          setData(resData.results)
-          setMessage('Search for more Music!!!')
-        } else {
+      if (search) {
+        try {
+          const res = await fetch(`https://itunes.apple.com/search?term=${search}`)
+          const resData = await res.json()
+          console.log(resData.results[0])
+          if (resData.results.length > 0) {
+            setData(resData.results)
+            setMessage('Search for more Music!!!')
+          } else {
+            
+          }
+        } catch (err) {
+          console.log(err)
           setMessage("Oopsie, that's not a valid search ...")
         }
-      } catch (err) {
-        console.log(err)
       }
     }
     fetchData()
